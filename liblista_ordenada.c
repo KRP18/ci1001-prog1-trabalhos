886d0e52a037103600637f8cd6b5f4af7c1cef86
Kauan da Rosa Paulino <55677667+KRPLAB@users.noreply.github.com>
2023-06-01 15:46:00 -0300

    indentação corrigida

--- liblista_ordenada.c
+++ liblista_ordenada.c
@@ -30,11 +30,11 @@ int lista_vazia(lista_t *l)
 {
 	return !l->ini;
 }
- 
+
 void lista_destroi(lista_t **l)
 {
 	nodo_t *aux;
-	
+
 	while(!lista_vazia((*l)))
 	{
 		aux = (*l)->ini;
@@ -42,61 +42,59 @@ void lista_destroi(lista_t **l)
 		free(aux->elemento);
 		free(aux);
 	}
-	
+
 	free((*l));
 	(*l) = NULL;
 }
 
 void elemento_destroi(elemento_t **elemento)
 {
-    if (elemento != NULL && *elemento != NULL) {
-        free(*elemento);
-        *elemento = NULL;
-    }
+	if (elemento != NULL && *elemento != NULL) {
+		free(*elemento);
+		*elemento = NULL;
+	}
 }
 
 int lista_insere_ordenado(lista_t *l, elemento_t *elemento)
 {
-    nodo_t *novo, *aux, *ant;
+	nodo_t *novo, *aux, *ant;
 
-    if (!(novo = malloc(sizeof(nodo_t))))
-        return 0;
+	if (!(novo = malloc(sizeof(nodo_t))))
+		return 0;
 
-    novo->elemento = elemento;
+	novo->elemento = elemento;
 
-    if (lista_vazia(l))
-    {
-        novo->prox = NULL;
-        l->ini = novo;
-        return 1;
-    }
+	if (lista_vazia(l))
+	{
+		novo->prox = NULL;
+		l->ini = novo;
+		return 1;
+	}
 
-    aux = l->ini;
-    ant = NULL;
-    while (aux != NULL && novo->elemento->chave > aux->elemento->chave)
-    {
-        ant = aux;
-        aux = aux->prox;
-    }
+	aux = l->ini;
+	ant = NULL;
+	while (aux != NULL && novo->elemento->chave > aux->elemento->chave)
+	{
+		ant = aux;
+		aux = aux->prox;
+	}
 
-    novo->prox = aux;
-    if (ant != NULL)
-        ant->prox = novo;
-    else
-        l->ini = novo;
+	novo->prox = aux;
+	if (ant != NULL)
+		ant->prox = novo;
+	else
+		l->ini = novo;
 
-    return 1;
+	return 1;
 }
 
 int lista_remove_ordenado(lista_t *l, elemento_t *elemento)
 {
 	nodo_t *aux, *temp;
-	
+
 	if(lista_vazia(l))
-	{
 		return 0;
-	}
-	
+
 	if(elemento->chave == l->ini->elemento->chave)
 	{
 		aux = l->ini;
@@ -104,17 +102,17 @@ int lista_remove_ordenado(lista_t *l, elemento_t *elemento)
 		free(aux);
 		return 1;
 	}
-	
+
 	aux = l->ini;
 	while(aux->prox != NULL && 
-		elemento->chave != aux->prox->elemento->chave)
+			elemento->chave != aux->prox->elemento->chave)
 	{
 		aux = aux->prox;
 	}
-	
+
 	if(aux->prox == NULL)
-			return 0;
-		
+		return 0;
+
 	temp = aux->prox;
 	aux->prox = temp->prox;
 	free(temp);
@@ -124,27 +122,23 @@ int lista_remove_ordenado(lista_t *l, elemento_t *elemento)
 int lista_search_ordenado(lista_t *l, elemento_t *elemento)
 {
 	nodo_t *aux;
-	
+
 	if(lista_vazia(l))
-	{
 		return 0;
-	}
-	
+
 	if(elemento->chave == l->ini->elemento->chave)
-	{
 		return 1;
-	}
-	
+
 	aux = l->ini;
 	while(aux->prox != NULL && 
-		elemento->chave != aux->prox->elemento->chave)
+			elemento->chave != aux->prox->elemento->chave)
 	{
 		aux = aux->prox;
 	}
-	
+
 	if(aux->prox == NULL || 
-		elemento->chave != aux->prox->elemento->chave)
-			return 0;
+			elemento->chave != aux->prox->elemento->chave)
+		return 0;
 
 	return 1;	
 }
